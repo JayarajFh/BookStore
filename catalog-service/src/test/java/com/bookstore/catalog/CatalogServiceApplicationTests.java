3336487f3279c3dbf7acb1c6ea37786bb6a00f4a
JayarajFh <jayrajfh97@gmail.com>
2025-12-21 00:26:04 +0530

    test cases fix

--- catalog-service/src/test/java/com/bookstore/catalog/CatalogServiceApplicationTests.java
+++ catalog-service/src/test/java/com/bookstore/catalog/CatalogServiceApplicationTests.java
@@ -3,8 +3,8 @@ package com.bookstore.catalog;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class CatalogServiceApplicationTests {
+
+class CatalogServiceApplicationTests extends AbstractIT {
 
     @Test
     void contextLoads() {}
